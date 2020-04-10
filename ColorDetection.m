5f96002d63cb87bc3d9eb4ae43e9b5c0deafd3bd
shmuglugg <62822861+shmuglugg@users.noreply.github.com>
2020-04-09 22:36:47 -0400

    switched scripts to functions

---
+++ ColorDetection.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = ColorDetection(inputArg1,inputArg2)
+%COLORDETECTION Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
---
+++ ResistCalc.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = ResistCalc(inputArg1,inputArg2)
+%RESISTCALC Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
