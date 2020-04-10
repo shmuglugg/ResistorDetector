7101ee0f6f1ab1e7da24553c9db731a8fd98ddf2
shmuglugg <62822861+shmuglugg@users.noreply.github.com>
2020-04-10 15:17:12 -0400

    some stufff

--- ColorCloud.m
+++ ColorCloud.m
@@ -1,11 +1,8 @@
-%%Read resistor picture file and output it, displays image variable size and class.
+%creates colorcloud of chosen image
 clc
 height = 300;
 width = 300;
 I = imread('res1clc.JPG');
 RGB = imresize(I, [height width]);
 
-
-
-
-
+colorcloud(RGB)
--- RGBColorDetection.m
+++ RGBColorDetection.m
@@ -1,4 +1,4 @@
-function [Final] = ColorDetection(RGB, height, width)
+function [Final] = RGBColorDetection(RGB, height, width)
 %COLORDETECTION takes input of an image and returns an image with
 %everything other than band colors masked out
 %COLOR BAND RGB VALUES:
@@ -14,53 +14,44 @@ function [Final] = ColorDetection(RGB, height, width)
 detected = 0;
 
 % Convert RGB image to chosen color space
-I = rgb2hsv(RGB);
-counter = 0;
+I = RGB;
+
 %Iterate through the image checking for HSV ranges and setting the RGB
 %pixels to preset color through color thresholding
 
 for row = 1:height
     for col = 1:width
-        
-%valuechecker
-counter = counter+1;
-if counter < 10
-I(row,col,1)
-I(row,col,2)
-I(row,col,3)
-end
-
-
 detected = 0;
 %BLACK
 % Define thresholds for channel 1 based on histogram settings
-Ch1Min = 0.045;
-Ch1Max = 0.115;
+Ch1Min = 0;
+Ch1Max = 60;
 
 % Define thresholds for channel 2 based on histogram settings
-Ch2Min = 0.243;
-Ch2Max = 0.944;
+Ch2Min = 0;
+Ch2Max = 60;
 
 % Define thresholds for channel 3 based on histogram settings
 Ch3Min = 0;
-Ch3Max = 0.173;
+Ch3Max = 60;
 
 %check to see if 
 if (Ch1Min <= I(row, col, 1)) && (Ch1Max >= I(row, col, 1)) && (Ch2Min <= I(row, col, 2)) && (Ch2Max >= I(row, col, 2)) && (Ch3Min <= I(row, col, 3)) && (Ch3Max >= I(row, col, 3))
-    I(row, col, 1) = 0;
-    I(row, col, 2) = 0;
-    I(row, col, 3) = 0;
+    RGB(row, col, 1) = 0;
+    RGB(row, col, 2) = 0;
+    RGB(row, col, 3) = 0;
     detected = 1;
 end
 
 %RED
-Ch1Min = 0.985;
-Ch1Max = 0.024;
-Ch2Min = 0.473;
-Ch2Max = 0.871;
-Ch3Min = 0.351;
-Ch3Max = 0.642;
+Ch1Min = 180;
+Ch1Max = 255;
+Ch2Min = 0;
+Ch2Max = 60;
+Ch3Min = 0;
+Ch3Max = 60;
 if (Ch1Min <= I(row, col, 1)) && (Ch1Max >= I(row, col, 1)) && (Ch2Min <= I(row, col, 2)) && (Ch2Max >= I(row, col, 2)) && (Ch3Min <= I(row, col, 3)) && (Ch3Max >= I(row, col, 3))
+    I(row, col, 1)
     RGB(row, col, 1) = 255;
     RGB(row, col, 2) = 0;
     RGB(row, col, 3) = 0;
@@ -68,12 +59,12 @@ if (Ch1Min <= I(row, col, 1)) && (Ch1Max >= I(row, col, 1)) && (Ch2Min <= I(row,
 end
 
 %BROWN
-Ch1Min = 0.011;
-Ch1Max = 0.090;
-Ch2Min = 0.398;
-Ch2Max = 0.831;
-Ch3Min = 0.179;
-Ch3Max = 0.328;
+Ch1Min = 123;
+Ch1Max = 183;
+Ch2Min = 72;
+Ch2Max = 132;
+Ch3Min = 21;
+Ch3Max = 81;
 if (Ch1Min <= I(row, col, 1)) && (Ch1Max >= I(row, col, 1)) && (Ch2Min <= I(row, col, 2)) && (Ch2Max >= I(row, col, 2)) && (Ch3Min <= I(row, col, 3)) && (Ch3Max >= I(row, col, 3))
     RGB(row, col, 1) = 153;
     RGB(row, col, 2) = 102;
@@ -82,12 +73,12 @@ if (Ch1Min <= I(row, col, 1)) && (Ch1Max >= I(row, col, 1)) && (Ch2Min <= I(row,
 end
 
 %ORANGE
-Ch1Min = 0.054;
-Ch1Max = 0.081;
-Ch2Min = 0.508;
-Ch2Max = 0.892;
-Ch3Min = 0.484;
-Ch3Max = 0.778;
+Ch1Min = 180;
+Ch1Max = 255;
+Ch2Min = 123;
+Ch2Max = 183;
+Ch3Min = 0;
+Ch3Max = 60;
 if (Ch1Min <= I(row, col, 1)) && (Ch1Max >= I(row, col, 1)) && (Ch2Min <= I(row, col, 2)) && (Ch2Max >= I(row, col, 2)) && (Ch3Min <= I(row, col, 3)) && (Ch3Max >= I(row, col, 3))
     RGB(row, col, 1) = 255;
     RGB(row, col, 2) = 153;
@@ -96,12 +87,12 @@ if (Ch1Min <= I(row, col, 1)) && (Ch1Max >= I(row, col, 1)) && (Ch2Min <= I(row,
 end
 
 %YELLOW
-Ch1Min = 0.125;
-Ch1Max = 0.139;
-Ch2Min = 0.669;
-Ch2Max = 0.825;
-Ch3Min = 0.632;
-Ch3Max = 0.826;
+Ch1Min = 180;
+Ch1Max = 255;
+Ch2Min = 180;
+Ch2Max = 255;
+Ch3Min = 0;
+Ch3Max = 60;
 if (Ch1Min <= I(row, col, 1)) && (Ch1Max >= I(row, col, 1)) && (Ch2Min <= I(row, col, 2)) && (Ch2Max >= I(row, col, 2)) && (Ch3Min <= I(row, col, 3)) && (Ch3Max >= I(row, col, 3))
     RGB(row, col, 1) = 255;
     RGB(row, col, 2) = 255;
@@ -110,12 +101,12 @@ if (Ch1Min <= I(row, col, 1)) && (Ch1Max >= I(row, col, 1)) && (Ch2Min <= I(row,
 end
 
 %GREEN
-Ch1Min = 0.265;
-Ch1Max = 0.331;
-Ch2Min = 0.403;
-Ch2Max = 0.636;
-Ch3Min = 0.302;
-Ch3Max = 0.469;
+Ch1Min = 0;
+Ch1Max = 60;
+Ch2Min = 180;
+Ch2Max = 255;
+Ch3Min = 0;
+Ch3Max = 60;
 if (Ch1Min <= I(row, col, 1)) && (Ch1Max >= I(row, col, 1)) && (Ch2Min <= I(row, col, 2)) && (Ch2Max >= I(row, col, 2)) && (Ch3Min <= I(row, col, 3)) && (Ch3Max >= I(row, col, 3))
     RGB(row, col, 1) = 0;
     RGB(row, col, 2) = 255;
@@ -125,12 +116,11 @@ end
 
 
 %MISSING BLUE AND PURPLE TO BE IMPLEMENTED
+
 if detected == 0
     RGB(row, col, 1) = 80;
     RGB(row, col, 2) = 80;
     RGB(row, col, 3) = 80;
-end
-    
     end
 end
 
